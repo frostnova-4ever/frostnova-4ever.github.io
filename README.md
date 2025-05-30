90d374fd84bd08b270700a9badc2bece7ed9f036
frostnova-4ever <3500531428@qq.com>
2025-05-31 02:22:01 +0800

    Initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# frostnova-4ever.github.io
-个人网站
-正在维修中
--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from "vue-router";
-import index from "../components/index.vue";
-import writer from "../components/admin/writer.vue";
-
-const routes = [{
-    path: '/',
-    name: 'index',
-    component: index
-}, ]
